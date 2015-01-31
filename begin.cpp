3ec531d1881f295e2abd2c343a621eadddfc6186
kursof <kurilchik.sofia@gmail.com>
2015-01-31 21:13:55 +0300

    firstcommit

---
+++ begin.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main ()
+
+{
+    int a,P,S;
+
+    std:: cout << "enter a " << std:: endl;
+    std:: cin >> a;//водится сторона
+
+    P=a*4;//подсчет периметра
+    S=a*a;// и площади
+
+    std:: cout << "perimeter is " << P << std:: endl;// вывод периметра
+    std:: cout << "square is " << S << std:: endl;//и площади
+
+    return 0;
+}
