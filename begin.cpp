4b67a11a86dd8c8f954909c64294166eff05a5db
ImMrSn <misha.sn@list.ru>
2015-02-01 10:28:15 +0300

    Fix

--- begin.cpp
+++ begin.cpp
@@ -7,9 +7,6 @@ int main ()
     std::cout << "Enter A:\n";
     std::cin >> a;
 
-    P = a * 4;
-    S = a * a;
-
     std::cout << "perimeter is " << a * 4 
     << "\nsquare is " << a * a << std::endl;
 
