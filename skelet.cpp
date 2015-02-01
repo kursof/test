c553cbbd5613c4d26ea22b87777336574cefeb52
ImMrSn <misha.sn@list.ru>
2015-02-01 20:10:44 +0300

    Add skelet.cpp
    Небольшой пример табуляции кода.

---
+++ skelet.cpp
@@ -0,0 +1,48 @@
+/* Описание программы. Что она должна делать.
+ * Можно с описание используемых алгоритмов.
+ * 
+ * Автор: Автор
+ * E-mail: е-mail автора
+ * Так же не плохо писать используемый компилятор.
+ */
+
+ #include <iostream>
+
+void nameFunction(int a, int b)
+{
+	// code
+}
+
+ int main()
+ {
+ 	const double pi = 3.141592;
+
+ 	int a, b, c;
+
+ 	std::cout << "Enter A: ", std::cin >> a;
+
+ 	std::cout << "Enter B: ", std::cin >> b;
+
+ 	std::cout << "Enter C: ";
+ 	std::cin >> c;
+
+ 	if (a > b)
+ 	{
+ 		// code
+ 	}
+ 	else
+ 	{
+ 		// code
+ 	}
+
+	for(int i = 0; i < ConstMaxN; ++i)
+	{
+		// code
+	}
+
+	std::cout << "Hello, world! And...\n"
+			<< a << '+' << b << "=" 
+			<< a + b << '\n';
+
+ 	return 0;
+ }
